adad7b93eddaa842e6a94c0a1b9113772fbb604b
nasifislam <n.islam@mybihs.org>
2016-11-10 23:01:50 +0000

    Initial commit

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    
+    // 
+  
+    $( "#emailButton" ).click(function() {
+        $("#emailButton").empty();
+      $("#emailButton").append('<input placeholder="Comment"></input>');
+    });
+        
+});
+
+
+
